a3ccbe7070f78c745ad15ab939adc3dec7a005e8
Victor Oliveira <victorlaertedoliveira@gmail.com>
2021-03-24 11:36:22 -0300

    Add xib navigation

--- ios/SoftwareEssentialsStoryBoard/SoftwareEssentialsStoryBoard/ByeViewController.swift
+++ ios/SoftwareEssentialsStoryBoard/SoftwareEssentialsStoryBoard/ByeViewController.swift
@@ -12,10 +12,13 @@ class ByeViewController: UIViewController {
   @IBOutlet weak var byeLabel: UILabel!
   
   override func viewDidLoad() {
-        super.viewDidLoad()
+      super.viewDidLoad()
 
-    byeLabel.text = "Byyyyyyyye"
+      byeLabel.text = "Byyyyyyyye"
         // Do any additional setup after loading the view.
     }
-
+  
+  @IBAction func byeButtonAction(_ sender: Any) {
+    self.navigationController?.pushViewController(XibViewController(), animated: true)
+  }
 }
